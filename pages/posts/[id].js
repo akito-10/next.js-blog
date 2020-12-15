6962d74e116cd088b6333863e60cfafcec136854
github akito-10 <akt.fukuda@gmail.com>
2020-12-15 16:39:35 +0900

    dateの表示変更

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,24 @@
 import Layout from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
+import Head from "next/head";
+import Date from "../../components/date";
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      <Date dateString={postData.date} />
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+    </Layout>
+  );
+}
 
 export async function getStaticProps({ params }) {
   const postData = await getPostData(params.id);
@@ -16,18 +35,4 @@ export async function getStaticPaths() {
     paths,
     fallback: false,
   };
-}
-
-export default function Post({ postData }) {
-  return (
-    <Layout>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
-      <br />
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-    </Layout>
-  );
 }
